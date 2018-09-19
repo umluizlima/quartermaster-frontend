cc36f848c3e31d2ebec5a364b9580930d09d9a4a
umluizlima <umluizlima@gmail.com>
2018-09-19 15:00:00 -0300

    Add category form

--- src/components/forms/CategoryForm.vue
+++ src/components/forms/CategoryForm.vue
@@ -1,15 +1,14 @@
 <template lang="html">
   <b-form ref="categoryForm"
-          @submit.prevent="">
+          @submit.prevent="handleSubmit">
 
     <b-form-group id="nameInputGroup"
                   label="Nome:"
                   label-for="nameInput">
       <b-form-input id="nameInput"
                     type="text"
-                    v-model="obj.name"
-                    required
-                    autofocus></b-form-input>
+                    v-model="form.name"
+                    required></b-form-input>
     </b-form-group>
 
     <b-form-group id="descriptionInputGroup"
@@ -17,18 +16,90 @@
                   label-for="descriptionInput">
       <b-form-input id="descriptionInput"
                     type="text"
-                    v-model="obj.description"></b-form-input>
+                    v-model="form.description"></b-form-input>
     </b-form-group>
+    <b-button v-if="objectId" type="submit" variant="warning">Confirmar</b-button>
+    <b-button v-else type="submit" variant="success">Confirmar</b-button>
   </b-form>
 </template>
 
 <script>
+import API from '@/utils/api'
+
 export default {
   name: 'CategoryForm',
   props: {
-    obj: {
+    objectId: {
+      type: Number,
+      required: false
+    },
+    alert: {
       type: Object,
       required: true
+    }
+  },
+  data () {
+    return {
+      api: new API('/categories'),
+      form: {}
+    }
+  },
+  mounted () {
+    this.clearForm()
+  },
+  watch: {
+    objectId: function () {
+      this.api.getOne(this.objectId)
+      .then((resp) => {
+        this.form = resp.data
+      })
+      .catch((err) => {
+        if (err.response.status === 404) {
+          this.alert.message = err.response.data.message
+          this.alert.show = true
+        }
+      })
+    }
+  },
+  methods: {
+    clearForm () {
+      this.form = {
+        name: null,
+        description: null
+      }
+    },
+    getObj () {
+      return {
+        name: this.form.name,
+        description: this.form.description
+      }
+    },
+    handleSubmit () {
+      if (this.objectId) {
+        this.api.update(this.objectId, this.getObj())
+        .then(() => {
+          this.clearForm()
+          this.$emit('submitted')
+        })
+        .catch((err) => {
+          if (err.response.status === 400) {
+            this.alert.message = err.response.data.message
+            this.alert.show = true
+          }
+        })
+      } else {
+        this.api.create(this.getObj())
+        .then(() => {
+          this.clearForm()
+          this.$emit('submitted')
+        })
+        .catch((err) => {
+          if (err.response.status === 400) {
+            this.alert.message = err.response.data.message
+            this.alert.show = true
+          }
+        })
+      }
     }
   }
 }

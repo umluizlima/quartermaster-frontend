5030ee952a1c6bb84a4767b62198cd6825a1bf02
umluizlima <umluizlima@gmail.com>
2018-10-02 15:54:05 -0300

    Add moment js dependency

--- src/main.js
+++ src/main.js
@@ -2,14 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import moment from 'moment'
+Vue.prototype.moment = moment
+
+
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(BootstrapVue)
 
 import { ClientTable } from 'vue-tables-2'
-
-Vue.use(BootstrapVue)
 Vue.use(ClientTable)
+
 Vue.config.productionTip = false
 
 new Vue({

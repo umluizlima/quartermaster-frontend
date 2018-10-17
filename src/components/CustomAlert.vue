798b01c06097dce141bfe8e6df6b5005b859b734
umluizlima <umluizlima@gmail.com>
2018-10-17 08:53:14 -0300

    Add generic alert accessed on store

---
+++ src/components/CustomAlert.vue
@@ -0,0 +1,16 @@
+<template lang="html">
+  <b-alert variant="danger"
+           dismissible
+           :show="!!$store.getters.error"
+           @dismissed="$store.commit('clearError')">
+     {{ this.$store.getters.error }}
+   </b-alert>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>

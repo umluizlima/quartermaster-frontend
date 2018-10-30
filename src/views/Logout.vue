692b0667655bedcce7c935a9ac83b033d059f84a
umluizlima <umluizlima@gmail.com>
2018-10-30 09:07:29 -0300

    Add logout view with functionality and redirect

---
+++ src/views/Logout.vue
@@ -0,0 +1,38 @@
+<template lang="html">
+  <div>
+    <h1>Saindo...</h1>
+    <p>Você será redirecionado em instantes.</p>
+  </div>
+</template>
+
+<script>
+import API from '@/utils/api'
+
+export default {
+  data () {
+    return {
+      api: new API('/users')
+    }
+  },
+  mounted () {
+    this.logout()
+  },
+  methods: {
+    logout () {
+      this.api.logout()
+        .then((resp) => {
+          this.$store.dispatch('logout')
+          this.$router.push('/')
+        })
+        .catch((err) => {
+          if (err.response.status === 400) {
+            this.$store.commit('setError', err.response.data.message)
+          }
+        })
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>

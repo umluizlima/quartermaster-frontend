f8ae69de7177d8d417c99afd66339f439666cbf7
umluizlima <umluizlima@gmail.com>
2018-10-30 09:16:54 -0300

    Add spacing between API methods

--- src/utils/api.js
+++ src/utils/api.js
@@ -6,9 +6,11 @@ const client = axios.create({
 })
 
 export default class API {
+
   constructor (resource) {
     this.resource = resource
   }
+
   async execute (method, resource, data) {
     let accessToken = localStorage.getItem('token') || ''
     return client({
@@ -20,28 +22,37 @@ export default class API {
         Authorization: `Bearer ${accessToken}`
       }
     })
+
   }
+
   get () {
     return this.execute('get', this.resource)
   }
+
   getOne (id) {
     return this.execute('get', `${this.resource}/${id}`)
   }
+
   getAll () {
     return this.execute('get', `${this.resource}/all`)
   }
+
   create (data) {
     return this.execute('post', `${this.resource}`, data)
   }
+
   update (id, data) {
     return this.execute('put', `${this.resource}/${id}`, data)
   }
+
   del (id) {
     return this.execute('delete', `${this.resource}/${id}`)
   }
+
   login (data) {
     return this.execute('post', '/login', data)
   }
+
   logout () {
     return this.execute('get', '/logout')
   }

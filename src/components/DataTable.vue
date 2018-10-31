da60a874ce595be9a4f771ad29d965e3b989eec5
umluizlima <umluizlima@gmail.com>
2018-10-31 15:30:55 -0300

    Add comments and get()/getAll() selector

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -1,20 +1,24 @@
 <template lang="html">
-  <div class="">
-    <v-client-table :data="data" :columns="columns" :options="options">
-      <b-btn slot="beforeTable"
+    <v-client-table class="datatable" :data="data" :columns="columns" :options="options">
+      <!-- Botão para adicionar novos objetos -->
+      <b-btn class="addBtn"
+             slot="beforeTable"
              variant="success"
              v-b-modal.createModal>Adicionar</b-btn>
 
+      <!-- Substitui valores booleanos por textos específicos -->
       <div v-for="b in bools" :slot="b.column" slot-scope="props">
         <p v-if="props.row[b.column]">{{ b.isTrue }}</p>
         <p v-else>{{ b.isFalse }}</p>
       </div>
 
+      <!-- Formata datetimes vindos do backend em formato ISO para o formato desejado -->
       <div v-for="dt in datetimes" :slot="dt" slot-scope="props">
         <p v-if="props.row[dt] != null">{{ moment(props.row[dt]).locale('pt-BR').format("DD/MM/YY - HH:mm") }}</p>
       </div>
 
-      <b-btn-group slot="actions" slot-scope="props">
+      <!-- Ações de editar e apagar em cada item da tabela -->
+      <b-btn-group class="actions" slot="actions" slot-scope="props">
         <b-btn v-b-modal.editModal
                variant="warning"
                @click="objectId = props.row.id">Editar</b-btn>
@@ -23,19 +27,31 @@
                v-b-modal.deleteModal
                @click="objectId = props.row.id">Apagar</b-btn>
       </b-btn-group>
+
+      <!-- Seletor para alternar entre itens atuais ou histórico completo, onde aplicável -->
+      <b-form-radio-group v-if="history.show"
+                          slot="afterTable"
+                          v-model="history.status"
+                          buttons
+                          button-variant="outline-primary"
+                          :options="history.options"
+                          @input="getData"
+                          required/>
+
+      <!-- Modais que são mostrados quando seus respectivos botões são apertados -->
+      <div class="modals" slot="afterTable">
+        <CreateModal :resource="api.resource"
+                     @ok="getData"/>
+
+        <EditModal :resource="api.resource"
+                   :id="objectId"
+                   @ok="getData"/>
+
+        <DeleteModal :resource="api.resource"
+                     :id="objectId"
+                     @ok="getData"/>
+      </div>
     </v-client-table>
-
-    <CreateModal :resource="api.resource"
-                 @ok="getData"/>
-
-    <EditModal :resource="api.resource"
-               :id="objectId"
-               @ok="getData"/>
-
-    <DeleteModal :resource="api.resource"
-                 :id="objectId"
-                 @ok="getData"/>
-  </div>
 </template>
 
 <script>
@@ -49,7 +65,7 @@ const conf = {
   perPageValues: [5, 10, 25],
   texts: {
     count: 'De {from} até {to} dos {count} resultados|{count} resultados|1 resultado',
-    filter: 'Pesquisar:',
+    filter: '',
     filterPlaceholder: 'Pesquisar...',
     limit: 'Resultados por página:',
     noResults: 'Sem resultados...'
@@ -66,6 +82,7 @@ export default {
       columns: [],
       bools: this.config.bools || [],
       datetimes: this.config.datetimes || [],
+      history: this.config.history || {},
       objectId: 0
     }
   },
@@ -87,7 +104,14 @@ export default {
   methods: {
     getData () {
       this.data = []
-      this.api.get()
+      console.log(this.history.status)
+      let request = null
+      if (this.history.status) {
+        request = this.api.getAll()
+      } else {
+        request = this.api.get()
+      }
+      request
         .then((resp) => {
           this.getForeignData(resp.data, this.config.foreign_keys)
           this.data = resp.data
@@ -113,12 +137,22 @@ export default {
     this.config.options.headings.actions = ''
     this.options = Object.assign(this.config.options, conf)
     this.getData()
-  },
-  updated () {
-    console.log(`objectId atual ${this.objectId}`)
   }
 }
 </script>
 
 <style lang="css">
+.datatable {
+  width: 90%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+@media screen and (min-width: 800px) {
+  .datatable {
+    max-width: 80%;
+  }
+}
 </style>

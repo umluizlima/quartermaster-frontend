19ce3275a9482262d2c44533de97d7064d5c05ef
umluizlima <umluizlima@gmail.com>
2018-11-27 16:22:15 -0200

    Change navbar tab grouping

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,17 +1,19 @@
 <template lang="html">
   <div id="navbar">
     <b-navbar toggleable="md" type="dark" variant="dark">
+      <b-navbar-brand>CDG Hub</b-navbar-brand>
       <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
-      <b-navbar-brand to="/">CDG Hub</b-navbar-brand>
       <b-collapse is-nav id="nav_collapse">
-        <b-navbar-nav v-if="$store.getters.isLoggedIn">
-          <!-- <b-nav-item to="/">Início</b-nav-item> -->
-          <b-nav-item to="/categorias">Categorias</b-nav-item>
-          <b-nav-item to="/itens">Itens</b-nav-item>
-          <b-nav-item to="/terceiros">Terceiros</b-nav-item>
-          <b-nav-item to="/emprestimos">Empréstimos</b-nav-item>
-          <b-nav-item to="/reservas">Reservas</b-nav-item>
-          <b-nav-item v-if="$store.getters.isAdmin" to="/usuarios">Usuários</b-nav-item>
+        <b-navbar-nav v-if="$store.getters.isLoggedIn" >
+          <b-nav-item to="/">INÍCIO</b-nav-item>
+          <b-nav-item-dropdown text="VISUALIZAR" left>
+            <b-dropdown-item to="/categorias">Categorias</b-dropdown-item>
+            <b-dropdown-item to="/itens">Itens</b-dropdown-item>
+            <b-dropdown-item to="/terceiros">Terceiros</b-dropdown-item>
+            <b-dropdown-item to="/emprestimos">Empréstimos</b-dropdown-item>
+            <b-dropdown-item to="/reservas">Reservas</b-dropdown-item>
+            <b-dropdown-item v-if="$store.getters.isAdmin" to="/usuarios">Usuários</b-dropdown-item>
+          </b-nav-item-dropdown>
         </b-navbar-nav>
 
         <!-- Right aligned nav items -->
@@ -19,7 +21,7 @@
           <b-nav-item-dropdown v-if="$store.getters.isLoggedIn" right>
             <!-- Using button-content slot -->
             <template slot="button-content">
-              <em>{{ $store.getters.user.first_name }}</em>
+              <em>{{ $store.getters.user.first_name }} {{ $store.getters.user.last_name }}</em>
             </template>
             <b-dropdown-item to="/conta">Minha conta</b-dropdown-item>
             <b-dropdown-item to="/sair">Sair</b-dropdown-item>

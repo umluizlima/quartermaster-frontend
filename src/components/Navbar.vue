95df1e7f766c19ecfc5a94ae0e74ccdfcd39217e
umluizlima <umluizlima@gmail.com>
2018-11-28 10:38:46 -0200

    Make logo clickable for home redirection

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template lang="html">
   <div id="navbar">
     <b-navbar toggleable="md" type="dark" variant="dark">
-      <b-navbar-brand>CDG Hub</b-navbar-brand>
+      <b-navbar-brand to="/">CDG Hub</b-navbar-brand>
       <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
       <b-collapse is-nav id="nav_collapse">
         <b-navbar-nav v-if="$store.getters.isLoggedIn" >

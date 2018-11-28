f2ed579d9b22f756c99ea53f0e86c114062571f9
umluizlima <umluizlima@gmail.com>
2018-11-28 11:13:01 -0200

    Change workboxPluginMode settings

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,14 @@ module.exports = {
   baseUrl: '/ic-cdg-hub-frontend/',
   pwa: {
     name: 'CDH Hub',
-    themeColor: '#343a40'
+    themeColor: '#343a40',
+    msTileColor: '#343a40',
+    // configure the workbox plugin
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: 'public/service-worker.js',
+      // ...other Workbox options...
+    }
   }
 }

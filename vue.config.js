50f07962b9d61bf70b4a0db14fc33762f01162f6
umluizlima <umluizlima@gmail.com>
2019-01-28 14:38:14 -0200

    Enable GenerateSW mode in vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,8 @@
 module.exports = {
   baseUrl: '/ic-cdg-hub-frontend/',
   pwa: {
-    name: 'CDH Hub',
     themeColor: '#343a40',
     msTileColor: '#343a40',
-    // configure the workbox plugin
-    // workboxPluginMode: 'InjectManifest',
-    // workboxOptions: {
-      // swSrc is required in InjectManifest mode.
-      // swSrc: 'public/service-worker.js',
-      // ...other Workbox options...
-    // }
+    workboxPluginMode: 'GenerateSW'
   }
 }
